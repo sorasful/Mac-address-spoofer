8f5b2b2bca69a28ecdba21a3feb4311d87d5280c
sorasful <teva.krief@hotmail.fr>
2018-04-02 14:37:15 +0200

    Can now change with mac address on network

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Mac address spoofer
 
+Requires Python3.6+
+
 This tool allows you to change your mac address with one you found when scanning the network.
 You can select on which interface you want to scan for hosts and then you can select which 
 mac address you want to use. 
@@ -16,4 +18,9 @@ For me, I need to use sudo because I have a permission denied, but I use virtual
 when I sudo I loose my virtualenv. In case you have the same trouble, I do the following :
 
 `sudo ./venv/bin/python3.6 spoof_mac_address.py`
+
+
+## N.B :
+
+Modifying mac address is a volatile action, so it will be changed until the next reboot.
 
--- spoof_mac_address.py
+++ spoof_mac_address.py
@@ -4,6 +4,7 @@ from scapy.sendrecv import srp
 
 import netifaces  # retrieves network interfaces easily
 
+import subprocess
 
 class Spoofer:
     """ Class used to perform actions like getting all the interfaces, find hosts, change mac address ..."""
@@ -14,11 +15,10 @@ class Spoofer:
 
         choosen_interface = interfaces[choosen_interface_num]
         self.interface = choosen_interface
-        print(netifaces.ifaddresses(choosen_interface))
         iface_infos = netifaces.ifaddresses(choosen_interface)
         self.ips = "{0}/24".format(iface_infos[2][0]['addr'])  # scan all network
 
-    def get_macs_and_ips(self):
+    def get_hosts_macs_and_ips(self):
         conf.verb = 0
         ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=self.ips), timeout=2, iface=self.interface, inter=0.1)
 
@@ -29,7 +29,23 @@ class Spoofer:
 
         return hosts
 
+    def alter_mac_address(self, interface, mac_address):
+        subprocess.run(f'ifconfig {interface} down', shell=True)
+        subprocess.run(f'ifconfig {interface} hw ether {mac_address}', shell=True)
+        subprocess.run(f'ifconfig {interface} up', shell=True)
+
+        return True
+
+    def change_current_mac_address(self):
+        """ Show all availables mac addresses and ip and let user select which mac address he wants to use from now. """
+        hosts = self.get_hosts_macs_and_ips()
+        host_mac, _ = hosts[eval(input("Choose which mac address you want to use : \n{0}\n choose : "
+                          .format("\n".join([f"{i} - {h[0]} - {h[1]}" for i, h in enumerate(hosts)]))))]
+
+        self.alter_mac_address(self.interface, host_mac)
+
+        print(f"Successfuly changed mac address to {host_mac}")
 
 if __name__ == "__main__":
     spoofer = Spoofer()
-    print(spoofer.get_macs_and_ips())
+    spoofer.change_current_mac_address()
